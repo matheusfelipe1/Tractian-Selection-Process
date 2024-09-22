b8212fdfafa84be9fdecc1d3f62bfd9a8d878cfb
Matheus Felipe Silva Santos <68447982+matheusfelipe1@users.noreply.github.com>
2024-09-22 10:10:11 -0300

    fix removing macos, windows and linux folders

---
+++ test/domain/company/unit_test/use_cases/get_companies_use_case_test.dart
@@ -0,0 +1,50 @@
+
+import 'package:flutter/material.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:traction_selection_proccess/src/core/utils/result.dart';
+import 'package:traction_selection_proccess/src/core/use_cases/use_cases.dart';
+import 'package:traction_selection_proccess/src/domain/company/entity/company_entity.dart';
+import 'package:traction_selection_proccess/src/domain/company/repository/company_repository.dart';
+import 'package:traction_selection_proccess/src/domain/company/use_cases/get_companies_use_case.dart';
+
+class CompanyRepositoryMock extends Mock implements CompanyRepository {}
+
+void main() {
+  late final CompanyRepository repository;
+  late final GetCompaniesUseCase getCompaniesUseCase;
+
+  setUp(() {
+    repository = CompanyRepositoryMock();
+    getCompaniesUseCase = GetCompaniesUseCase(repository);
+  });
+
+  test(
+    "Must return a list of companies",
+    () async {
+      when(() => repository.getCompanies()).thenAnswer(
+        (_) async => Result.success(
+          [
+            const CompanyEntity(id: "123", name: "Tractian: Inovação")
+          ]
+        ),
+      );
+
+      final result = await getCompaniesUseCase(NoParams());
+
+      expect(result, isA<Result<List<CompanyEntity>, Exception>>());
+      expect(result.isSuccess, equals(true));
+      result.proccessResult(
+        onSuccess: (data) {
+          expect(data.length, equals(1));
+          expect(data.first.id, equals("123"));
+          expect(data.first.name, equals("Tractian: Inovação"));
+        },
+        onFailure: (error) {
+          debugPrint("error on $error");
+          fail("Should not be called");
+        },
+      );
+    },
+  );
+}
