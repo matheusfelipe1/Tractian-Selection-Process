c0ca24b37502626b67b97c0c73a82c4883a46207
Matheus Felipe Silva Santos <68447982+matheusfelipe1@users.noreply.github.com>
2024-09-26 07:01:38 -0300

    fix waterfall organization

--- lib/app/core/constants/app_constants.dart
+++ lib/app/core/constants/app_constants.dart
@@ -1,12 +1,12 @@
 final class AppConstants {
-  static const String appName = 'Traction Selection Process';
-  static const String designSystemPackageName = 'design_system';
-  static const String locationId = "locationId";
-  static const String companyId = "companyId";
   static const String id = "id";
   static const String name = "name";
   static const String parentId = "parentId";
   static const String sensorId = "sensorId";
   static const String sensorStatus = "status";
+  static const String companyId = "companyId";
   static const String sensorType = "sensorType";
+  static const String locationId = "locationId";
+  static const String appName = 'Traction Selection Process';
+  static const String designSystemPackageName = 'design_system';
 }
